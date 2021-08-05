3bd79207cc4f9bd9c5fabf65f04287467855d219
Marco Kretz <mk@marco-kretz.de>
2021-08-05 16:09:38 +0200

    init

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import "../styles/app.css";
+</script>
+
+<header>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">ALLWOW</a>
+        </div>
+    </nav>
+</header>
+
+<main class="pt-4">
+    <div class="container">
+        <slot></slot>
+    </div>
+</main>
+
+<footer></footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+    export async function load({ fetch }) {
+        const res = await fetch('/api.json');
+
+        if (res.ok) {
+            return { props: { data: await res.json() } };
+        }
+
+        return { status: res.status, error: new Error() };
+    }
+</script>
+
+<script>
+    export let data;
+</script>
+
+<h2>AddOn-Manager</h2>
+
+<div class="row gx-3 gy-3">
+    {#each data.addonManagers as manager}
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{manager.name}</h5>
+                    <p class="card-text">{manager.description}</p>
+                    <a href={manager.url} class="btn btn-primary" ref="noopener" target="_blank">Download</a>
+                </div>
+            </div>
+        </div>
+    {/each}
+</div>
